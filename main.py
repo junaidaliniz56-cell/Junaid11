a185b83a54e689c242257556e06945c1c7f458d0
Junaid Niz <junaidaliniz56@gmail.com>
2026-01-14 20:52:38 +0500

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sqlite3
 import os
 from telebot import types
 
-BOT_TOKEN = "8546188939:AAGCchjT0fnBRmgeKVz87S1i7cIkhVOfZHI"
+BOT_TOKEN = "8433897615:AAFGshJ6sD9R_31niIvObq_hExAVXrFmr6A"
 ADMINS = [7011937754]
 
 # بوٹ کو تھریڈز کے ساتھ چلائیں تاکہ سپیڈ تیز ہو
